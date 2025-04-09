f5690384f34f26d581b709def245c2ec37a9857b
Yuliia Kravchenko <yukravch@bess-f2r3s12.clusters.42paris.fr>
2025-04-09 15:56:40 +0200

    added print bits (for int)

--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: yukravch <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 10:49:33 by yukravch          #+#    #+#             */
-/*   Updated: 2025/03/25 16:38:42 by yukravch         ###   ########.fr       */
+/*   Updated: 2025/04/09 15:54:59 by yukravch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,5 +67,5 @@ void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
-
+void	ft_print_bits(int c);
 #endif
